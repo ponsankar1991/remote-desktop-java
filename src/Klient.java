e3ce924a84a186389200dc63451eff47d78ffa13
wojtek30100 <wojtek30100@gmail.com>
2012-06-17 12:03:00 +0000

    tu bedzie dodane przesylanie klikniec

--- src/Klient.java
+++ src/Klient.java
@@ -4,9 +4,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.InputStream;
 import java.net.Socket;
 import java.text.DateFormat;
 import java.util.Date;
@@ -20,24 +23,50 @@ public class Klient extends JFrame {
 
 
 	private Socket socket;
-	private ObjectInputStream is = null;
+	private InputStream is = null;
 	private BufferedImage image = null;
 	
 	
 	class Receiver extends Thread {
 		@Override
 		public void run() {
-			Screen b = null;
+			while (!socket.isClosed()) {
+				getScreenshot();
+			}
+		}
+		
+		private void getScreenshot() {
+			int bytesRead = 0;
+			int current = 0;
 			try {
-				while ((b = (Screen) is.readObject()) != null) {
-					Klient.this.image = b.getImage();
-					Klient.this.repaint();
-					
-					System.out.println("Odebrano screen"+DateFormat.getTimeInstance().format(new Date()));
-				}
+				FileOutputStream fos = new FileOutputStream("rscreen.jpg");
+			    BufferedOutputStream bos = new BufferedOutputStream(fos);
+			    DataInputStream dis = new DataInputStream(Klient.this.is);
+			    int filesize = dis.readInt();
+			    byte [] mybytearray  = new byte [filesize];
+			    bytesRead = Klient.this.is.read(mybytearray,0,mybytearray.length);
+			    current = bytesRead;
+
+			    do {
+			       bytesRead =
+			          is.read(mybytearray, current, (mybytearray.length-current));
+			       if(bytesRead >= 0) current += bytesRead;
+			    } while(bytesRead > -1 && current != filesize);
+
+			    bos.write(mybytearray, 0 , current);
+			    bos.flush();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		    
+		    try {
+				Klient.this.image = ImageIO.read(new File("rscreen.jpg"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		    Klient.this.repaint();
+				
+			System.out.println("Odebrano screen"+DateFormat.getTimeInstance().format(new Date()));
 		}
 	}
 	
@@ -70,7 +99,7 @@ public class Klient extends JFrame {
 				try {
 					socket = new Socket("192.168.1.4", 1235);
 					try {
-						is = new ObjectInputStream(socket.getInputStream());
+						is = socket.getInputStream();
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}
