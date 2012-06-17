3bd70a8a8c4fbe81a2d425d1cf77bd78141aad8e
wojtek30100@gmail.com <wojtek30100@gmail.com@7bf690f2-386a-1990-a1b1-373252ad60cf>
2012-06-17 16:50:35 +0000

    klawiatura juz dziala

---
+++ branches/klawirka/src/Klient.java
@@ -0,0 +1,171 @@
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class Klient extends JFrame implements MouseListener {
+
+
+	private Socket socket;
+	private InputStream is = null;
+	private ObjectOutputStream os = null;
+	private BufferedImage image = null;
+	
+	
+	class Receiver extends Thread {
+		@Override
+		public void run() {
+			while (!socket.isClosed()) {
+				getScreenshot();
+			}
+		}
+		
+		private void getScreenshot() {
+			int bytesRead = 0;
+			int current = 0;
+			try {
+				FileOutputStream fos = new FileOutputStream("rscreen.jpg");
+			    BufferedOutputStream bos = new BufferedOutputStream(fos);
+			    DataInputStream dis = new DataInputStream(Klient.this.is);
+			    int filesize = dis.readInt();
+			    byte [] mybytearray  = new byte [filesize];
+			    bytesRead = Klient.this.is.read(mybytearray,0,mybytearray.length);
+			    current = bytesRead;
+
+			    do {
+			       bytesRead =
+			          is.read(mybytearray, current, (mybytearray.length-current));
+			       if(bytesRead >= 0) current += bytesRead;
+			    } while(bytesRead > -1 && current != filesize);
+
+			    bos.write(mybytearray, 0 , current);
+			    bos.flush();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		    
+		    try {
+				Klient.this.image = ImageIO.read(new File("rscreen.jpg"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		    Klient.this.repaint();
+				
+			System.out.println("Odebrano screen"+DateFormat.getTimeInstance().format(new Date()));
+		}
+	}
+	
+	
+	public Klient() {
+		
+		createGUI();
+		this.addMouseListener(this);
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				if (socket != null) {
+					try {
+						socket.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+				
+				System.exit(0);
+			}
+		});
+	}
+
+	private void createGUI() {
+		JButton connect = new JButton("CONNECT");
+		connect.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					socket = new Socket("192.168.1.4", 1235);
+					try {
+						is = socket.getInputStream();
+						os = new ObjectOutputStream(socket.getOutputStream());
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					
+					new Receiver().start();
+					
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+				
+			}
+		});
+		
+		
+		this.getContentPane().setLayout(null);
+		this.getContentPane().add(connect);
+		connect.setBounds(0, 0, 200, 100);
+		//this.getContentPane().add(shot);
+		//this.pack();
+		
+		this.setUndecorated(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(0, 0, 1280, 800);
+		this.setVisible(true);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.drawImage(image, 0, 0, null);
+	}
+	
+	public static void main(String[] args) {
+		new Klient().setVisible(true);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		try {
+			os.writeObject(new Point(e.getX(), e.getY()));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
+
+	
+
+}
---
+++ branches/klawirka/src/Serwer.java
@@ -0,0 +1,141 @@
+import java.awt.AWTException;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.InputEvent;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class Serwer extends JFrame {
+	
+	/**
+	 * @author Wojtek
+	 * Klasa odpowiedzialna z robienie zrzutów ekranu i przesyłanie ich
+	 * strumieniem.
+	 */
+	class ScreenShoter extends TimerTask {
+		
+		private Rectangle screenRect;
+		
+		public ScreenShoter() {
+			screenRect = new Rectangle(Toolkit.getDefaultToolkit()
+					.getScreenSize());
+		}
+
+		@Override
+		public void run() {
+			try {
+				File screen = captureScreenShot();
+				
+				
+				int fileLength = (int)screen.length();
+				byte [] mybytearray  = new byte [fileLength];
+				BufferedInputStream bis = new BufferedInputStream(new FileInputStream(screen));
+				bis.read(mybytearray,0,mybytearray.length);
+				
+				DataOutputStream dos = new DataOutputStream(Serwer.this.os);
+				dos.writeInt(fileLength);
+				Serwer.this.os.write(mybytearray,0,mybytearray.length);
+				
+			    os.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		private File captureScreenShot() {
+			BufferedImage capture;
+			File f = new File("screen.jpg");; 
+			File compressed = new File("compressed.jpg");
+			try {
+				capture = robot.createScreenCapture(screenRect);
+				ImageIO.write(capture, "jpg", f);
+				
+				
+				ImageReader imgRdr = ImageIO.getImageReadersByFormatName("jpg").next();
+				ImageWriter imgWrtr = ImageIO.getImageWritersByFormatName("jpg").next();
+				ImageInputStream imgInStrm = ImageIO.createImageInputStream(f);
+				ImageOutputStream imgOutStrm = ImageIO.createImageOutputStream(compressed);
+				imgRdr.setInput(imgInStrm);
+				imgWrtr.setOutput(imgOutStrm);
+				IIOImage iioImg = new IIOImage(imgRdr.read(0), null, null); 
+
+				// set compression level 
+				ImageWriteParam jpgWrtPrm = imgWrtr.getDefaultWriteParam();
+				jpgWrtPrm.setCompressionMode(JPEGImageWriteParam.MODE_EXPLICIT);
+				jpgWrtPrm.setCompressionQuality(0.1f);
+
+				// write out JPEG
+				imgWrtr.write(null, iioImg, jpgWrtPrm);
+
+				// clean up 
+				imgInStrm.close();
+				imgOutStrm.close();
+
+				imgWrtr.dispose();
+				imgRdr.dispose();
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+			return compressed;
+		}
+		
+	}
+	
+	private ServerSocket ss;
+	private ObjectInputStream is;
+	private OutputStream os;
+	private Robot robot;
+	
+	public Serwer() throws Exception {
+		ss = new ServerSocket(1235);
+		Socket socket = ss.accept();
+		is = new ObjectInputStream(socket.getInputStream());
+		os = socket.getOutputStream();
+		try {
+			robot = new Robot();
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		
+		Timer t = new Timer();
+		t.schedule(new ScreenShoter(), 1000, 1000);
+		
+		Point p = null;
+		while ((p = (Point) is.readObject()) != null) {
+			robot.mouseMove(p.x, p.y);
+			robot.mousePress(InputEvent.BUTTON1_MASK);
+            robot.mouseRelease(InputEvent.BUTTON1_MASK);
+            System.out.println("klik!");
+		}
+	}
+	
+	public static void main(String... args) throws Exception {
+		new Serwer();
+	}
+
+}
